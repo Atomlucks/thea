28bfa7034c0973440cb37f3bef157efd127439c9
Atomlucks <82471490+Atomlucks@users.noreply.github.com>
2021-04-13 20:43:53 -0500

    mi primer commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/bootstrap-grid.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+<body class="Loging">
+
+  <div class="row ">
+    <div class="col-xl-8">
+    <img src="img/image.png"class="image" alt="Responsive image">
+    </div>
+    <div class="col-xl-4">
+        <div class="container margin1">
+            <div class="Logo-thea4x">
+                <img src="img/logo-thea-4-x.png"class="Logo-thea4x">
+            </div>
+            <h2 class="registro">REGISTRO</h2>
+            <form action="">
+                <div class="form-group mx-sm-5 mb-2">
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Nombre">
+                </div>
+                <div class="row mx-sm-5 mb-2">
+                    <div class="col pr-0 pl-0">
+                        <input type="text" class="form-control" placeholder="Primer apellido">
+                    </div>
+                    <div class="col pr-0">
+                        <input type="text" class="form-control" placeholder="Segundo apellido">
+                    </div>
+                </div>
+                <div class="form-group mx-sm-5 mb-2">
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Subespecialidad">
+                </div>
+                <div class="form-group mx-sm-5 mb-2">
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Ciudad">
+                </div>
+                <div class="form-group mx-sm-5 mb-2">
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Hospital o Institución de Ejercicio">
+                </div>
+                <div class="form-group mx-sm-5 mb-2">
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                </div>
+                <div class="form-check mx-sm-5 mb-2">
+                    <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="1" aria-label="...">
+                    <span>Acepto</span><a href=""> términos y condiciones</a> <span>de este aviso</span>
+                </div>
+                <div class="form-group mx-sm-5 mb-2">
+                <button type="submit" class="btn-oli-ingresar">Ingresar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+  </div>  
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="./js/bootstrap.bundle.js"></script>
+<script src="./js/bootstrap.bundle.min.js"></script>
+<script src="./js/bootstrap.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ zyter/zyter-quiz.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/zyter-quiz.css">
+    <link rel="stylesheet" href="../css/bootstrap-grid.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+<body >
+<div class="Loging">
+  <div class="row w-100 Rectangle2 justify-content-center">
+        <div class="col-xl-6" style="align-self: center;">  
+            <img src="../img/logo-thea-4-x.png"class="Logo-thea4x">
+        </div>
+        <div class="col-xl-3" style="align-self: center; text-align-last: end;">
+            <span class="Dr-Christian-Olivar" >Dr. Cristian Olivares</span>
+            <span class="green"></span>
+        </div>  
+  </div>
+<div class="row w-100 Rectangle ">
+    <div class="row w-100 justify-content-center mt-5">
+        <div class="col-7" style="align-self: center; padding: 0  !important;">
+             <p>INSTRUCCIONES</p>
+             <P>Lea cuidadosamente cada una de las afirmaciones y elija la que considere que más se asemeja a las características de ZYTER®.</P>
+             <P>Como conocedor del antibiótico ZYTER®, nos gustaría hacerle unas preguntas.</P>
+        </div>
+        <div class="col-3 Rectangulo-img text-center" style="align-self: center;">
+            <img src="../img/logo-zyter-4-x.png"class="Logo-zyter4x img-fluid">
+        </div>
+    </div>
+    <div class="row w-100 justify-content-center mt-5"> 
+        <div class="col-10 preguntas-index" style="align-self: center;">
+            <span class="Preguntas">Preguntas</span>
+        </div>
+        <div class="col-10 Rectangle3" style="text-align:center">        
+        </div>
+        <div class="col-9 footer1">
+            <span>©Laboratorios Théa. Todos los derechos reservados</span> <br>
+            <a href="">Aviso de Privacidad</a>
+        </div>
+    </div>
+</div>
+</div>    
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="../js/bootstrap.bundle.js"></script>
+<script src="../js/bootstrap.bundle.min.js"></script>
+<script src="../js/bootstrap.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+
+</body>
+</html>
